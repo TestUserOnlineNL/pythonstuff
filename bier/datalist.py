cd84525162bc5df33dbbd146cb721b3c5be4484a
TestUserOnlineNL <testuser.onlinenl@gmail.com>
2024-08-21 14:42:37 +0200

    many code changes

--- bier/datalist.py
+++ bier/datalist.py
@@ -1,10 +1,5 @@
-hl = ["id","title","year","movie"]
-hl2 = ["id","title","year","movie_picture"]
-hl3 = ["id","movie_title","year","picture"]
-columns = [0] * len(hl)
-
-
-def column_width(dl=[]):
+# get max data width per element
+def column_width(dl):
 	el = 0
 	while el < len(dl):
 		cw = len(dl[el])
@@ -14,21 +9,25 @@ def column_width(dl=[]):
 
 	return columns
 
-print(column_width(hl))
-print(column_width(hl2))
-print(column_width(hl3))
 
-
+# fill-up column
 def column_stuffing(data, data_width):
 	col_fill = "." * data_width
 	cw = len(col_fill)
 	dw = len(data)
 	f = cw - dw
 	if f >= 0:
-	    return (col_fill[0:f]+data)
+	    return col_fill[0:f]+data
 	else:
-		return("Error")
+		return "Error"
 
 
-result = column_stuffing("crashtest",12)
-print(result)
+# read csv file
+with open("movies.csv","r",encoding="UTF8") as test_data:
+
+	for line in test_data:
+		line_list = line.split(",")
+		columns = [0] * len(line_list)
+		show = column_width(line_list)
+		print(show)
+		print(line_list[1].strip('"'),len(line_list[1].strip('"')))
