9333d3790b464f4829e59c25d72ed86de661323f
TestUserOnlineNL <testuser.onlinenl@gmail.com>
2024-08-28 13:58:42 +0200

    adjustments made to kladblok.py

--- movies/kladblok.py
+++ movies/kladblok.py
@@ -16,7 +16,7 @@ with open(filename,"r",encoding="UTF8") as data_in:
             collection.append(filtered_line)
 
 
-# convert list to dictionary
+# convert list to dictionary in memory
 stapel = {}
 for r,row in enumerate(collection, 1):
     coll = {};
@@ -25,3 +25,34 @@ for r,row in enumerate(collection, 1):
 
     movie = "movie" + str(r)
     stapel[movie] = coll
+
+
+# export to fixed width format
+def columns_width(bieren):
+    columns = [0] * 3
+
+    for row in bieren:
+        for i, item in enumerate(row):
+            columns[i] = max(columns[i],len(str(item)))
+    return columns
+
+
+def columns_fixing(bieren,max_width):
+    store = []
+    for row in bieren:
+        regel = ""
+        for i, item in enumerate(row):
+            text = str(item).ljust(max_width[i])
+            regel = regel + text
+        store.append(regel)
+    return store
+
+
+def show_fixed_data():
+    sizes = columns_width(collection)
+    fixed_data = columns_fixing(collection,sizes)
+
+    for textline in fixed_data:
+        print(textline)
+
+show_fixed_data()
