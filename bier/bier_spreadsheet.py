58fb78eec807c95ec4abe4555e03e1286aee01fd
TestUserOnlineNL <testuser.onlinenl@gmail.com>
2024-09-23 15:00:00 +0200

    changes made;

--- bier/bier_spreadsheet.py
+++ bier/bier_spreadsheet.py
@@ -1,9 +1,11 @@
 # bier_spreadsheet.py
-sheet = []
+dict_sheet_row = {}
 with open('./bier.csv') as csv_file:
 	for ln,line in enumerate(csv_file,1):
+		
 		mylist=(line.strip("\n").split(","))
-		sheet.append(mylist)
+		# print(len(mylist))
+		dict_sheet_row.update({f'row {ln}':mylist})
 
 
-print(sheet)
+print(dict_sheet_row)
